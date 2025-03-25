394de0ea288c6051356cf970d7dc3eae620be91a
Muhammad Hussain <k213584@nu.edu.pk>
2025-03-26 00:12:21 +0500

    Added demo code

---
+++ src/GenerateQR/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GenerateQR
+{
+    public class QRGenerator
+    {
+        public static string Generate(string input)
+        {
+            return $"QR:{input}";
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(QRGenerator.Generate("HelloWorld"));
+        }
+    }
+}
